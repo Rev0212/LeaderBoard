2a08fa3ade749ee9957395e795eccc468b300de5
A-Akhil <akhilrahul70@gmail.com>
2025-04-01 16:51:24 +0530

    added 100 for inactive students

--- frontend/src/components/reports/StudentsSection.jsx
+++ frontend/src/components/reports/StudentsSection.jsx
@@ -411,6 +411,7 @@ const StudentsSection = ({ detailedStudentPerformance, inactiveStudents, availab
               <option value={10}>10</option>
               <option value={25}>25</option>
               <option value={50}>50</option>
+              <option value={100}>100</option>
             </select>
           </div>
         </div>
