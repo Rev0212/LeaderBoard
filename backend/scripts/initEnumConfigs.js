311a6f8226eb6410252af725248fd20595fd5947
rev0212 <ra6857@srmist.edu.in>
2025-04-10 12:24:39 +0530

    Working Config in Admin

--- backend/scripts/initEnumConfigs.js
+++ backend/scripts/initEnumConfigs.js
@@ -4,7 +4,7 @@ const PointsConfig = require('../models/pointsConfig.model');
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.DB_CONNECT)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
